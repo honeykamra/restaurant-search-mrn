53e7e321ddd36d9a2582e113753840409300c286
Honey Kamra <kamrahny@gmail.com>
2021-08-12 12:20:07 +0530

    manually added initial code

---
+++ models/restaurant-details.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const RestaurantDetailsSchema = mongoose.Schema({
+    id: Number,
+    name: String,
+    phoneNumber: Number,
+    location: {
+        lat: Number,
+        long: Number,
+        city: String,
+        state: String,
+        streetAddress: String,
+    },
+    cuisines: [String],
+    menu: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'RestaurantMenu'
+    }]
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('RestaurantDetail', RestaurantDetailsSchema, 'restaurantDetails');
